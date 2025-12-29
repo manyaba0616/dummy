17170b01f896ec63e20864939e34eb3948839928
manyaba0616 <manyaba0616@gmail.com>
2025-12-28 21:38:36 -0800

    Clean up

--- src/dummy/crew.py
+++ src/dummy/crew.py
@@ -19,36 +19,6 @@ class Dummy():
     
     # If you would like to add tools to your agents, you can learn more about it here:
     # https://docs.crewai.com/concepts/agents#agent-tools
-    # @agent
-    # def researcher(self) -> Agent:
-    #     return Agent(
-    #         config=self.agents_config['researcher'], # type: ignore[index]
-    #         verbose=True
-    #     )
-
-    # @agent
-    # def reporting_analyst(self) -> Agent:
-    #     return Agent(
-    #         config=self.agents_config['reporting_analyst'], # type: ignore[index]
-    #         verbose=True
-    #     )
-
-    # # To learn more about structured task outputs,
-    # # task dependencies, and task callbacks, check out the documentation:
-    # # https://docs.crewai.com/concepts/tasks#overview-of-a-task
-    # @task
-    # def research_task(self) -> Task:
-    #     return Task(
-    #         config=self.tasks_config['research_task'], # type: ignore[index]
-    #     )
-
-    # @task
-    # def reporting_task(self) -> Task:
-    #     return Task(
-    #         config=self.tasks_config['reporting_task'], # type: ignore[index]
-    #         output_file='report.md'
-    #     )
-
     @agent
     def analytical_debater(self) -> Agent:
         return Agent(
